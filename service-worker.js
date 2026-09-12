bf0441498d0a957938ca5d65b45cc91a34687f65
mdsalmafarsi60852963741-cyber <mdsalmafarsi60852963741@gmail.com>
2026-09-12 16:08:38 +0600

    Update service worker cache version and logic

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,17 @@
-const CACHE_NAME = "hifz-kontak-v1";
-
-const APP_FILES = [
-  "./",
-  "./index.html",
-  "./manifest.json",
- "./file_00000000485c8211bf81374240c59fe9.png" 
-];
+const CACHE_NAME = "hifz-kontak-v2";
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(APP_FILES);
-    })
-  );
-  self.skipWaiting();
+    self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys
-          .filter((key) => key !== CACHE_NAME)
-          .map((key) => caches.delete(key))
-      );
-    })
-  );
-  self.clients.claim();
+    event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
 });
